4a41069dd2aec919674add49a07f87b162b8d744
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2022-01-28 03:23:16 +0330

    Add getInput()

--- main.cpp
+++ main.cpp
@@ -13,10 +13,37 @@ void getAlg(){
     scanf("%u",&alg);
 }
 
+void getInput(vector<Dvec> &ps){
+    cout<<"Enter List of Tasks. format: processBurstTime-processID\ntype \"end-end\" to end entering"<<endl;
+    bool even=false;
+    string temp;
+    Dvec temp_;
+
+    while (true){
+
+        if(even)
+            getline(cin, temp);
+        else
+            getline(cin, temp,DLM);
+
+        if(temp=="end")break;
+
+        temp_.push_back(stod(temp));
+
+        if(even){
+            ps.push_back(Dvec(temp_)) ;
+            temp_.clear();
+        }
+        even=!even;
+    }
+}
 int main() {
     vector<Dvec> processes;
 
     getAlg();
+    getInput(processes);
+
+
 
     return 0;
 }
